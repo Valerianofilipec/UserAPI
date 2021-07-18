0a30f047ee4480a2a9c85ee67e06feeafd736726
Valeriano Filipe Calológio <valeriofilipec@gmail.com>
2021-07-18 03:29:33 +0100

    definição de rotas

--- src/routes.ts
+++ src/routes.ts
@@ -9,4 +9,16 @@ router.get("/teste", (request, response) => response.json({
     "username": "JaguarNegro"
 }))
 
+//[ ] Uma rota que cadastra um usuário (cada usuário tem nome, email, senha e nome de usuário. As senhas precisam ter pelo menos 6 caracteres). Não é possível cadastrar usuários com o mesmo nome de usuário ou email.
+router.put('/user/sign-up', ()=> console.log("signing up a new user"))
+
+//[ ] Uma rota para ver um perfil do usuário (essa rota só mostra nome, email e nome de usuário).
+router.get('/user/:id', () => console.log(`user ${id} `)))
+
+//[ ] Uma rota para editar um usuário.
+router.post('/user/:id', ()=> console.log('edit user '))
+
+//[ ] Uma rota para deletar um usuário. A rota de deletar não apaga um registro no banco de dados, mas só marca o usuário como deletado(soft delete).
+router.delete('user/:id&:password', ()=> console.log('delete a user(password required'))
+
 export {router}
